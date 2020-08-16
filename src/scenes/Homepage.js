a85d341c6e7515f01f97fc0b6daf0258e7e5aa8d
fadhilansyah25 <fadhilansyah25@gmail.com>
2020-08-16 08:33:42 +0700

    convert header

---
+++ src/scenes/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Header from 'parts/Header';
+
+export default function Homepage() {
+    return (
+        <div>
+            <Header></Header>
+        </div>
+    )
+}
